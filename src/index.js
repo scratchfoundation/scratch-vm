0b70ceb93583be760cca2cfef62c52583fb3aafe
TheBrokenRail <connor24nolan@live.com>
2016-09-04 12:01:45 -0400

    Move newProject to index.js

--- src/index.js
+++ src/index.js
@@ -110,7 +110,21 @@ VirtualMachine.prototype.postIOData = function (device, data) {
  */
 VirtualMachine.prototype.loadProject = function (json) {
     if (!json) {
-        newProject(this.runtime);
+        var blocks = new Blocks();
+        var sprite = new Sprite(blocks);
+        sprite.name = 'Sprite1';
+        sprite.costumes.push({skin: '09dc888b0b7df19f70d81588ae73420e.svg',
+            name: 'costume1',
+            bitmapResolution: 1,
+            rotationCenterX: 47,
+            rotationCenterY: 55}); 
+        var target = sprite.createClone();
+        this.runtime.targets.push(target);
+        target.x = 0;
+        target.y = 0;
+        target.direction = 90;
+        target.size = 100;
+        target.visible = true;
         this.editingTarget = this.runtime.targets[0];
         this.emitTargetsUpdate();
         this.emitWorkspaceUpdate();
