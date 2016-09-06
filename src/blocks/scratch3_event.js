1d46b2a2d019de91eb1c74e154a489772c42afb4
Tim Mickel <tim.mickel@gmail.com>
2016-09-06 15:26:51 -0400

    Green flag skips clones

--- src/blocks/scratch3_event.js
+++ src/blocks/scratch3_event.js
@@ -23,7 +23,8 @@ Scratch3EventBlocks.prototype.getPrimitives = function() {
 Scratch3EventBlocks.prototype.getHats = function () {
     return {
         'event_whenflagclicked': {
-            restartExistingThreads: true
+            restartExistingThreads: true,
+            skipClones: true
         },
         'event_whenkeypressed': {
             restartExistingThreads: false
