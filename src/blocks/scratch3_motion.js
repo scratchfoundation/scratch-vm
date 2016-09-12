91cdec15474d86b960f64ce952c0ff432d524fbe
Tim Mickel <tim.mickel@gmail.com>
2016-09-12 13:26:10 -0400

    Implement glide (#154)

--- src/blocks/scratch3_motion.js
+++ src/blocks/scratch3_motion.js
@@ -1,5 +1,6 @@
 var Cast = require('../util/cast');
 var MathUtil = require('../util/math-util');
+var Timer = require('../util/timer');
 
 function Scratch3MotionBlocks(runtime) {
     /**
@@ -20,6 +21,7 @@ Scratch3MotionBlocks.prototype.getPrimitives = function() {
         'motion_turnright': this.turnRight,
         'motion_turnleft': this.turnLeft,
         'motion_pointindirection': this.pointInDirection,
+        'motion_glidesecstoxy': this.glide,
         'motion_changexby': this.changeX,
         'motion_setx': this.setX,
         'motion_changeyby': this.changeY,
@@ -57,6 +59,41 @@ Scratch3MotionBlocks.prototype.turnLeft = function (args, util) {
 Scratch3MotionBlocks.prototype.pointInDirection = function (args, util) {
     var direction = Cast.toNumber(args.DIRECTION);
     util.target.setDirection(direction);
+};
+
+Scratch3MotionBlocks.prototype.glide = function (args, util) {
+    if (!util.stackFrame.timer) {
+        // First time: save data for future use.
+        util.stackFrame.timer = new Timer();
+        util.stackFrame.timer.start();
+        util.stackFrame.duration = Cast.toNumber(args.SECS);
+        util.stackFrame.startX = util.target.x;
+        util.stackFrame.startY = util.target.y;
+        util.stackFrame.endX = Cast.toNumber(args.X);
+        util.stackFrame.endY = Cast.toNumber(args.Y);
+        if (util.stackFrame.duration <= 0) {
+            // Duration too short to glide.
+            util.target.setXY(util.stackFrame.endX, util.stackFrame.endY);
+            return;
+        }
+        util.yieldFrame();
+    } else {
+        var timeElapsed = util.stackFrame.timer.timeElapsed();
+        if (timeElapsed < util.stackFrame.duration * 1000) {
+            // In progress: move to intermediate position.
+            var frac = timeElapsed / (util.stackFrame.duration * 1000);
+            var dx = frac * (util.stackFrame.endX - util.stackFrame.startX);
+            var dy = frac * (util.stackFrame.endY - util.stackFrame.startY);
+            util.target.setXY(
+                util.stackFrame.startX + dx,
+                util.stackFrame.startY + dy
+            );
+            util.yieldFrame();
+        } else {
+            // Finished: move to final position.
+            util.target.setXY(util.stackFrame.endX, util.stackFrame.endY);
+        }
+    }
 };
 
 Scratch3MotionBlocks.prototype.changeX = function (args, util) {
