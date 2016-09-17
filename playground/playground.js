7f5d2a405a60db3304a3f899f13c6462ba01b176
TheBrokenRail <connor24nolan@live.com>
2016-09-17 10:11:56 -0400

    Changed Error Message

--- playground/playground.js
+++ playground/playground.js
@@ -12,7 +12,7 @@ var loadProject = function () {
                 window.vm.loadProject(this.responseText);
             } else {
                 window.vm.createEmptyProject();
-                alert('Error: ' + r.status);
+                alert('Failed To Retrieve Project From Server\nError Code: ' + r.status);
             }
         }
     };
