1577182db18551ab9d91472b930d518204344020
TheBrokenRail <connor24nolan@live.com>
2016-09-17 10:05:56 -0400

    Add Imported Message

--- playground/playground.js
+++ playground/playground.js
@@ -10,8 +10,10 @@ var loadProject = function () {
         if (this.readyState === 4) {
             if (r.status === 200) {
                 window.vm.loadProject(this.responseText);
+                alert('Imported');
             } else {
                 window.vm.createEmptyProject();
+                alert('Error: ' + r.status + '\nCreated Empty Project');
             }
         }
     };
@@ -40,7 +42,7 @@ window.onload = function() {
             tabRenderExplorer.style.display = 'block';
             tabThreadExplorer.style.display = 'none';
             tabImportExport.style.display = 'none';
-            alert('Imported!');
+            alert('Imported');
         }
         reader.readAsText(file);
     });
