30fc4ea961a463a718cb65ee99572f78fdefc22e
dekrain <dawidkrainski8@gmail.com>
2016-10-21 16:53:25 +0200

    Parse blocks

--- src/projects/sb3export.js
+++ src/projects/sb3export.js
@@ -14,7 +14,7 @@
  * [ ] Costumes
  * [X] Effects
  * [X] Clones
- * [ ] Blocks
+ * [X] Blocks
  */
 
 
@@ -38,13 +38,14 @@ function sb3export (runtime) {
  * @return {string} A JSON string representing package.json file
  */
 function projectjson (runtime) {
-	var json = Object.create(null),
+    var json = Object.create(null),
         target, effect, i = 0;
     json.name = 'new Project'; // No project name changing for now
     json.sprites = [];
     for (target in runtime.targets) {
-    	if (!target.isOriginal) continue; // Don't save multiple copies
-        var sprite = target.sprite;
+        if (!target.isOriginal) continue; // Don't save multiple copies
+        var sprite = target.sprite, q,
+            blocks = {}, queueInps = [], queueCons = [];
         json.sprites[i] = {};
         json.sprites[i].name = sprite.name;
         json.sprites[i].costumes = [];
@@ -58,10 +59,41 @@ function projectjson (runtime) {
             oClone.y = clone.y;
             oClone.effects = {};
             for (effect in clone.effects) {
-        	    oClone.effects[effect] = target.effects[effect];
+                oClone.effects[effect] = target.effects[effect];
             }
             oClone.current_costume = clone.currentCostume;
-            oClone.blocks = {};
+        });
+        json.spites[i].blocks = [];
+        Object.keys(sprite.blocks._blocks).forEach(function(block, idx){
+            block = sprite.blocks._blocks[block];
+            blocks[block.id] = idx;
+            json.sprites[i].blocks[idx] = {};
+            json.sprites[i].blocks[idx].ID = idx;
+            json.sprites[i].blocks[idx].opcode = block.opcode;
+            json.sprites[i].blocks[idx].fields = {};
+            for (var field in block.fields) {
+                json.sprites[i].blocks[idx].fields[field] = block.fields[field];
+            }
+            json.sprites[i].blocks[idx].inputs = {};
+            for (var input in block.inputs) {
+                queueInps.push({
+                    value: block.inputs[input].block || block.inputs[input].shadow,
+                    input: input,
+                    block: idx
+                });
+            }
+            json.sprites[i].blocks[idx].isField = block.field;
+            if (block.next) queueCons.push({value: block.next, block: idx});
+        });
+        for (q in queueInps) {
+            json.sprites[i].blocks[q.block].inputs[q.input] = blocks[q.value];
+        }
+        for (q in queueCons) {
+            json.sprites[i].blocks[q.block].next = blocks[q.value];
+        }
+        json.sprites[i].scripts = [];
+        sprite.blocks._scripts.forEach(function(ID){
+            json.sprites[i].scripts[ID] = blocks[ID];
         });
         ++i;
     }
@@ -74,13 +106,13 @@ function projectjson (runtime) {
  * @return {Promise} Promise containing ZIP string
  */
 function exportZIP (runtime) {
-	var zip = new JSZip(),
-	    pack = sb3export(runtime),
-	    entry;
-	for (entry in pack) {
-		zip.file(entry, pack[entry].contents, pack[entry].options);
-	}
-	return zip.generateAsync({type: 'binarystring'});
+    var zip = new JSZip(),
+        pack = sb3export(runtime),
+        entry;
+    for (entry in pack) {
+        zip.file(entry, pack[entry].contents, pack[entry].options);
+    }
+    return zip.generateAsync({type: 'binarystring'});
 };
 
 sb3export.exportZIP = exportZIP;
