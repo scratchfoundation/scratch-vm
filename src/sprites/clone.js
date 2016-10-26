36f40887ebdf881968a2d69e290e55547051d783
Tim Mickel <tim.mickel@gmail.com>
2016-10-26 09:55:25 -0400

    Place clones behind sprite

--- src/sprites/clone.js
+++ src/sprites/clone.js
@@ -505,6 +505,18 @@ Clone.prototype.goBackLayers = function (nLayers) {
 };
 
 /**
+ * Move behind some other clone.
+ * @param {!Clone} otherClone Other clone to move behind.
+ */
+Clone.prototype.goBehindOtherClone = function (otherClone) {
+    if (this.renderer) {
+        var otherLayer = this.renderer.setDrawableOrder(
+            otherClone.drawableID, 0, true);
+        this.renderer.setDrawableOrder(this.drawableID, otherLayer);
+    }
+};
+
+/**
  * Keep a desired position within a fence.
  * @param {number} newX New desired X position.
  * @param {number} newY New desired Y position.
@@ -557,6 +569,7 @@ Clone.prototype.makeClone = function () {
     }
     this.runtime.changeCloneCounter(1);
     var newClone = this.sprite.createClone();
+    // Copy all properties.
     newClone.x = this.x;
     newClone.y = this.y;
     newClone.direction = this.direction;
@@ -569,6 +582,8 @@ Clone.prototype.makeClone = function () {
     newClone.lists = JSON.parse(JSON.stringify(this.lists));
     newClone.initDrawable();
     newClone.updateAllDrawableProperties();
+    // Place clone behind the current target.
+    newClone.goBehindOtherClone(this);
     return newClone;
 };
 
