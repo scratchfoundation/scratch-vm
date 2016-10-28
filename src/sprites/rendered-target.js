31279499a6560b9aa8ad74765ab198d5d1eebf59
Cosmic Web Services <andrewrodebaugh@cosmicsearch.org>
2016-10-28 10:55:29 -0400

    Update rendered-target.js

--- src/sprites/rendered-target.js
+++ src/sprites/rendered-target.js
@@ -250,7 +250,8 @@ RenderedTarget.prototype.setSize = function (size) {
         return;
     }
     // Keep size between 5% and 535%.
-    this.size = MathUtil.clamp(size, 5, 535);
+    //@todo get minimum size
+    this.size = MathUtil.clamp(size, 5, math.min((stage.height / costume.height) * 100, (stage.width / costume.width) * 100));
     if (this.renderer) {
         var renderedDirectionScale = this._getRenderedDirectionAndScale();
         this.renderer.updateDrawableProperties(this.drawableID, {
