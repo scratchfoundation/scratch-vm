737e0fadd02396566376d2a305783b041966827c
TheBrokenRail <connor24nolan@live.com>
2016-11-13 12:15:15 -0500

    Add Basic Custom Reporter Code

--- src/blocks/scratch3_procedures.js
+++ src/blocks/scratch3_procedures.js
@@ -4,6 +4,7 @@ var Scratch3ProcedureBlocks = function (runtime) {
      * @type {Runtime}
      */
     this.runtime = runtime;
+    this.thread = 0;
 };
 
 /**
@@ -14,7 +15,9 @@ Scratch3ProcedureBlocks.prototype.getPrimitives = function () {
     return {
         procedures_defnoreturn: this.defNoReturn,
         procedures_callnoreturn: this.callNoReturn,
-        procedures_param: this.param
+        procedures_callreturn: this.callReturn,
+        procedures_param: this.param,
+        procedures_report: this.report
     };
 };
 
@@ -26,19 +29,44 @@ Scratch3ProcedureBlocks.prototype.callNoReturn = function (args, util) {
     if (!util.stackFrame.executed) {
         var procedureCode = args.mutation.proccode;
         var paramNames = util.getProcedureParamNames(procedureCode);
+        util.stackFrame.thread = this.thread;
+        this.thread = this.thread + 1;
         for (var i = 0; i < paramNames.length; i++) {
             if (args.hasOwnProperty('input' + i)) {
                 util.pushParam(paramNames[i], args['input' + i]);
             }
         }
         util.stackFrame.executed = true;
-        util.startProcedure(procedureCode);
+        util.startProcedure(procedureCode, util.stackFrame.thread);
+    }
+};
+
+Scratch3ProcedureBlocks.prototype.callReturn = function (args, util) {
+    if (!util.stackFrame.executed) {
+        var procedureCode = args.mutation.proccode;
+        var paramNames = util.getProcedureParamNames(procedureCode);
+        util.stackFrame.thread = this.thread;
+        this.thread = this.thread + 1;
+        for (var i = 0; i < paramNames.length; i++) {
+            if (args.hasOwnProperty('input' + i)) {
+                util.pushParam(paramNames[i], args['input' + i]);
+            }
+        }
+        util.stackFrame.executed = true;
+        util.startProcedure(procedureCode, util.stackFrame.thread);
+    }
+    if (util.isProcedureDone(util.stackFrame.thread)) {
+        return util.getProcedureReturn(util.stackFrame.thread);
     }
 };
 
 Scratch3ProcedureBlocks.prototype.param = function (args, util) {
     var value = util.getParam(args.mutation.paramname);
     return value;
+};
+
+Scratch3ProcedureBlocks.prototype.report = function (args, util) {
+    util.setProcedureReturn(args.VALUE);
 };
 
 module.exports = Scratch3ProcedureBlocks;
