8540983899b497d519bdee27a96f786b858ffdf5
Ray <rschamp@users.noreply.github.com>
2017-02-10 19:03:23 +0000

    Build for fb181d5ce34642bd135a2008c074a63308408667

--- scratch-vm.js
+++ scratch-vm.js
@@ -19607,7 +19607,7 @@ Keyboard.prototype._keyCodeToScratchKey = function (keyCode) {
     case 39: return 'right arrow';
     case 40: return 'down arrow';
     }
-    return null;
+    return '';
 };
 
 /**
