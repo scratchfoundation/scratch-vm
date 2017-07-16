84ac66d6e5b3770bc2135f1c013857d6844b1499
Christopher Willis-Ford <cwillisf@media.mit.edu>
2017-07-16 00:14:03 -0700

    Fix exception message

--- test/fixtures/dispatch-test-service.js
+++ test/fixtures/dispatch-test-service.js
@@ -8,7 +8,7 @@ class DispatchTestService {
     }
 
     throwException () {
-        throw new Error('This is a test exception thrown by LocalDispatchTest');
+        throw new Error('This is a test exception thrown by DispatchTest');
     }
 
     close () {
