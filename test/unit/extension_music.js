1b520567e2f36fc6e42591460768d87efe559493
Eric Rosenbaum <eric.rosenbaum@gmail.com>
2017-11-20 19:05:21 -0500

    Fix unit test for set instrument

--- test/unit/extension_music.js
+++ test/unit/extension_music.js
@@ -1,17 +1,8 @@
 const test = require('tap').test;
 const Music = require('../../src/extensions/scratch3_music/index.js');
-let playedDrum;
-let playedInstrument;
-const runtime = {
-    audioEngine: {
-        numInstruments: 3,
-        instrumentPlayer: {
-            loadInstrument: instrument => (playedInstrument = instrument)
-        }
-    }
-};
+const runtime = Object.create(null);
+
 const blocks = new Music(runtime);
-blocks._playDrumNum = (util, drum) => (playedDrum = drum);
 
 const util = {
     stackFrame: Object.create(null),
@@ -22,6 +13,10 @@ const util = {
 };
 
 test('playDrum uses 1-indexing and wrap clamps', t => {
+    // Stub playDrumNum
+    let playedDrum;
+    blocks._playDrumNum = (_util, drum) => (playedDrum = drum);
+
     let args = {DRUM: 1};
     blocks.playDrumForBeats(args, util);
     t.strictEqual(playedDrum, 0);
@@ -35,15 +30,16 @@ test('playDrum uses 1-indexing and wrap clamps', t => {
 
 test('setInstrument uses 1-indexing and wrap clamps', t => {
     // Stub getMusicState
-    blocks._getMusicState = () => ({});
+    const state = {currentInstrument: 0};
+    blocks._getMusicState = () => state;
 
     let args = {INSTRUMENT: 1};
     blocks.setInstrument(args, util);
-    t.strictEqual(playedInstrument, 0);
+    t.strictEqual(state.currentInstrument, 0);
 
-    args = {INSTRUMENT: runtime.audioEngine.numInstruments + 1};
+    args = {INSTRUMENT: blocks.INSTRUMENT_INFO.length + 1};
     blocks.setInstrument(args, util);
-    t.strictEqual(playedInstrument, 0);
+    t.strictEqual(state.currentInstrument, 0);
 
     t.end();
 });
