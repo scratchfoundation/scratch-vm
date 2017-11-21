d3894b66ad09a5013403970fa0ec49a5da5cf94d
Andrew <andrewsliwinski@acm.org>
2017-11-21 01:24:00 +0000

    Build for 8395fffe528c161b38dda20974b4447943bc7f7c

--- vendor.js
+++ vendor.js
@@ -24409,10 +24409,27 @@ function isUndefined(arg) {
 	Author Tobias Koppers @sokra
 */
 module.exports = function(src) {
-	if (typeof execScript !== "undefined")
-		execScript(src);
-	else
-		eval.call(null, src);
+	function log(error) {
+		(typeof console !== "undefined")
+		&& (console.error || console.log)("[Script Loader]", error);
+	}
+
+	// Check for IE =< 8
+	function isIE() {
+		return typeof attachEvent !== "undefined" && typeof addEventListener === "undefined";
+	}
+
+	try {
+		if (typeof execScript !== "undefined" && isIE()) {
+			execScript(src);
+		} else if (typeof eval !== "undefined") {
+			eval.call(null, src);
+		} else {
+			log("EvalError: No eval function available");
+		}
+	} catch (error) {
+		log(error);
+	}
 }
 
 
