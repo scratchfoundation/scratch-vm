1633b1b2ef82df761a5f3a29010559cef271da7d
Andrew <andrewsliwinski@acm.org>
2017-11-28 12:49:21 +0000

    Build for 421a92cad16777e4297dcc8b7354822817cf8171

--- vendor.js
+++ vendor.js
@@ -1,4 +1,14 @@
-/******/ (function(modules) { // webpackBootstrap
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["VirtualMachine"] = factory();
+	else
+		root["VirtualMachine"] = factory();
+})(this, function() {
+return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
 /******/
@@ -22748,4 +22758,5 @@ function isnan (val) {
 /***/ })
 
 /******/ });
+});
 //# sourceMappingURL=vendor.js.map
