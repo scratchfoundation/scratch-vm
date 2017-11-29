6b00f97804d00db2fa9b1fdb98e7577101252cf9
Paul Kaplan <pkaplan@media.mit.edu>
2017-11-29 15:46:04 -0500

    Fix test

--- test/unit/blocks_procedures.js
+++ test/unit/blocks_procedures.js
@@ -16,7 +16,7 @@ test('calling a custom block with no definition does not throw', t => {
         }
     };
     const util = {
-        getProcedureParamNamesAndIds: () => null,
+        getProcedureParamNamesAndIds: () => [null, null],
         stackFrame: {
             executed: false
         }
