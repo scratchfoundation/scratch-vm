430dbf0bd6cb13c6f0d0d0337111fa2c1e12e2c0
Florrie <towerofnix@gmail.com>
2018-01-23 11:53:23 -0400

    Remove old dead import file

--- src/import/load-old-text-costume.js
+++
@@ -1,6 +0,0 @@
-// @todo this file's name is a little bit unwieldly; should it be merged with load-costume.js (which could also contain common code between the two functions)?
-
-const StringUtil = require('../util/string-util');
-const log = require('../util/log');
-
-module.exports = loadOldTextCostume;
