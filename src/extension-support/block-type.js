d3352ac54d6137d540f0d40fe92518cac088a6aa
Christopher Willis-Ford <cwillisf@media.mit.edu>
2018-03-20 11:23:55 -0700

    Add JSDoc tags for ArgumentType & BlockType

--- src/extension-support/block-type.js
+++ src/extension-support/block-type.js
@@ -1,8 +1,31 @@
+/**
+ * Types of block
+ * @enum {string}
+ */
 const BlockType = {
+    /**
+     * Boolean reporter with hexagonal shape
+     */
     BOOLEAN: 'Boolean',
+
+    /**
+     * Command block
+     */
     COMMAND: 'command',
+
+    /**
+     * Specialized command block which may or may not run a child branch
+     */
     CONDITIONAL: 'conditional',
+
+    /**
+     * Hat block which conditionally starts a block stack
+     */
     HAT: 'hat',
+
+    /**
+     * General reporter with numeric or string value
+     */
     REPORTER: 'reporter'
 };
 
