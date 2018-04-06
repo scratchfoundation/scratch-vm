58c9b11dd3cd887c6ee734b9be8bcacb8cc9daef
Karishma Chadha <kchadha@scratch.mit.edu>
2018-04-06 11:38:41 -0400

    Update test to use new function.

--- test/unit/serialization_sb3.js
+++ test/unit/serialization_sb3.js
@@ -2,12 +2,12 @@ const test = require('tap').test;
 const path = require('path');
 const VirtualMachine = require('../../src/index');
 const sb3 = require('../../src/serialization/sb3');
-const extract = require('../fixtures/extract');
+const readFileToBuffer = require('../fixtures/readProjectFile').readFileToBuffer;
 const projectPath = path.resolve(__dirname, '../fixtures/clone-cleanup.sb2');
 
 test('serialize', t => {
     const vm = new VirtualMachine();
-    vm.loadProject(extract(projectPath))
+    vm.loadProject(readFileToBuffer(projectPath))
         .then(() => {
             const result = sb3.serialize(vm.runtime);
             // @todo Analyze
