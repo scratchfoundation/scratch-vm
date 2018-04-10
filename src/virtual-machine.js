da539ea6cd0b0efb4692a7ae8c668f0dddbb80de
Paul Kaplan <pkaplan@media.mit.edu>
2018-04-10 14:31:49 -0400

    Only select the original target after a drag.
    This allows you to still drag clones around, but when you stop dragging,
    the original sprite gets selected.

--- src/virtual-machine.js
+++ src/virtual-machine.js
@@ -900,7 +900,7 @@ class VirtualMachine extends EventEmitter {
         if (target) {
             this._dragTarget = null;
             target.stopDrag();
-            this.setEditingTarget(target.id);
+            this.setEditingTarget(target.sprite.clones[0].id);
         }
     }
 
