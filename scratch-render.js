986f1c40e178dab1220fc370d84fec1a4be97a33
DD <liudi08@gmail.com>
2018-04-26 18:02:42 +0000

    Build for 5f20609dc1b124b0916b791d1a3b85f199736fb9

--- scratch-render.js
+++ scratch-render.js
@@ -19654,6 +19654,35 @@ var RenderWebGL = function (_EventEmitter) {
 
             var newSkin = new SVGSkin(skinId, this);
             newSkin.setSVG(svgData, rotationCenter);
+            this._reskin(skinId, newSkin);
+        }
+
+        /**
+         * Update an existing bitmap skin, or create a bitmap skin if the previous skin was not bitmap.
+         * @param {!int} skinId the ID for the skin to change.
+         * @param {!string} imgData - new bitmap to use.
+         * @param {?Array<number>} rotationCenter Optional: rotation center of the skin. If not supplied, the center of the
+         * skin will be used
+         */
+
+    }, {
+        key: 'updateBitmapSkin',
+        value: function updateBitmapSkin(skinId, imgData, rotationCenter) {
+            // Divide rotation center by 2 and set bitmap resolution = 2 because all images coming from paint editor
+            // are double resolution
+            var updatedRotationCenter = rotationCenter ? [Math.floor(rotationCenter[0] / 2), Math.floor(rotationCenter[1] / 2)] : null;
+            if (this._allSkins[skinId] instanceof BitmapSkin) {
+                this._allSkins[skinId].setBitmap(imgData, 2, updatedRotationCenter);
+                return;
+            }
+
+            var newSkin = new BitmapSkin(skinId, this);
+            newSkin.setBitmap(imgData, 2, updatedRotationCenter);
+            this._reskin(skinId, newSkin);
+        }
+    }, {
+        key: '_reskin',
+        value: function _reskin(skinId, newSkin) {
             var oldSkin = this._allSkins[skinId];
             this._allSkins[skinId] = newSkin;
 
@@ -19668,6 +19697,8 @@ var RenderWebGL = function (_EventEmitter) {
 
                     if (drawable && drawable.skin === oldSkin) {
                         drawable.skin = newSkin;
+                        drawable.setConvexHullDirty();
+                        drawable.setTransformDirty();
                     }
                 }
             } catch (err) {
