85da0ac5e3b13c1270d2a42a8f257a29e4067cd8
picklesrus <picklesrus@users.noreply.github.com>
2018-05-04 17:23:01 -0700

    Initial version of the speech extension.  There is certainly still some stuff to figure out but the core of it is here.

---
+++ src/extensions/scratch3_speech/manifest.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'speech-rec-start.mp3': require('!buffer-loader!./assets/speech-rec-start.mp3'),
+    'speech-rec-end.mp3': require('!buffer-loader!./assets/speech-rec-end.mp3')
+};
