82fd6f0d2f0e3aff702915b4d0731ce3818b204d
Kreg Hanning <khanning@gmail.com>
2018-05-14 13:52:49 -0400

    Add micro:bit Scratch 3.0 extension (#1113)
    * Add micro:bit Scratch 3.0 extension
    
    * Fix lint errors in micro:bit extension
    
    * Fix doc. Incorrect return type
    
    * Check for valid pin in when pin connected block
    
    * Drop mapping function
    
    * Drop question mark from tilt hat block
    
    * Generate list of symbols from object keys
    
    * Trim display text block to max 20 characters

--- src/extension-support/extension-manager.js
+++ src/extension-support/extension-manager.js
@@ -10,6 +10,7 @@ const BlockType = require('./block-type');
 const Scratch3PenBlocks = require('../extensions/scratch3_pen');
 const Scratch3WeDo2Blocks = require('../extensions/scratch3_wedo2');
 const Scratch3MusicBlocks = require('../extensions/scratch3_music');
+const Scratch3MicroBitBlocks = require('../extensions/scratch3_microbit');
 const Scratch3TranslateBlocks = require('../extensions/scratch3_translate');
 const Scratch3VideoSensingBlocks = require('../extensions/scratch3_video_sensing');
 
@@ -17,6 +18,7 @@ const builtinExtensions = {
     pen: Scratch3PenBlocks,
     wedo2: Scratch3WeDo2Blocks,
     music: Scratch3MusicBlocks,
+    microbit: Scratch3MicroBitBlocks,
     translate: Scratch3TranslateBlocks,
     videoSensing: Scratch3VideoSensingBlocks
 };
