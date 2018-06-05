3a697ae0acdf6fcbcc0ea19805e14f2ecc712ec7
Paul Kaplan <pkaplan@media.mit.edu>
2018-06-05 10:46:06 -0400

    Add top-level API for getting and setting variable values

--- src/virtual-machine.js
+++ src/virtual-machine.js
@@ -1071,6 +1071,42 @@ class VirtualMachine extends EventEmitter {
             this.editingTarget.postSpriteInfo(data);
         }
     }
+
+    /**
+     * Set a target variables value if it exists. Return whether it succeeded.
+     * @param {!string} targetId ID of the target which owns the variable.
+     * @param {!string} variableId ID of the variable to set.
+     * @param {!*} value The new value of that variable.
+     * @returns {boolean} whether the target and variable were found and updated.
+     */
+    setVariableValue (targetId, variableId, value) {
+        const target = this.runtime.getTargetById(targetId);
+        if (target) {
+            const variable = target.lookupVariableById(variableId);
+            if (variable) {
+                variable.value = value;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Set a target variables value if it exists. Return whether it succeeded.
+     * @param {!string} targetId ID of the target which owns the variable.
+     * @param {!string} variableId ID of the variable to set.
+     * @returns {?*} The value of the variable, or null if it could not be looked up.
+     */
+    getVariableValue (targetId, variableId) {
+        const target = this.runtime.getTargetById(targetId);
+        if (target) {
+            const variable = target.lookupVariableById(variableId);
+            if (variable) {
+                return variable.value;
+            }
+        }
+        return null;
+    }
 }
 
 module.exports = VirtualMachine;
