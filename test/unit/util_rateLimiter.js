769156c9af416430c5e0cc20621471d2b99fde43
Eric Rosenbaum <eric.rosenbaum@gmail.com>
2018-08-28 17:49:44 -0400

    Fix unit test

--- test/unit/util_rateLimiter.js
+++ test/unit/util_rateLimiter.js
@@ -1,18 +1,25 @@
 const test = require('tap').test;
 const RateLimiter = require('../../src/util/rateLimiter.js');
 
-test('limit', t => {
+test('rate limiter', t => {
     const rate = 30;
     const limiter = new RateLimiter(rate);
-    t.equal(limiter.getCount(), rate);
+
+    // The rate limiter starts with a number of tokens equal to the max rate
+    t.equal(limiter._count, rate);
+
+    // Running okayToSend rate times uses up all of the tokens
     for (let i = 0; i < rate; i++) {
-        t.true(limiter.check());
-        t.equal(limiter.getCount(), rate - (i + 1));
+        t.true(limiter.okayToSend());
+        t.equal(limiter._count, rate - (i + 1));
     }
-    t.false(limiter.check());
+    t.false(limiter.okayToSend());
+
+    // After a delay of one second divided by the max rate, we should have exactly
+    // one more token to use.
     setTimeout(() => {
-        t.true(limiter.check());
-        t.false(limiter.check());
+        t.true(limiter.okayToSend());
+        t.false(limiter.okayToSend());
         t.end();
     }, 1000 / rate);
 });
