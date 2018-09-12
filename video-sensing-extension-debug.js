efbe9c7d0e0c7094b5f598f0f78b7544c0303237
Andrew Sliwinski <andrewsliwinski@acm.org>
2018-09-12 16:12:52 +0000

    Build for d64f91f96bac27c75e086d8807954b37fbac2d1e

--- video-sensing-extension-debug.js
+++ video-sensing-extension-debug.js
@@ -82,7 +82,7 @@ var VirtualMachine =
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 474);
+/******/ 	return __webpack_require__(__webpack_require__.s = 473);
 /******/ })
 /************************************************************************/
 /******/ ({
@@ -201,18 +201,18 @@ module.exports = {
 
 /***/ }),
 
-/***/ 474:
+/***/ 473:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 /* WEBPACK VAR INJECTION */(function(global) {
 
-module.exports = global["Scratch3VideoSensingDebug"] = __webpack_require__(475);
+module.exports = global["Scratch3VideoSensingDebug"] = __webpack_require__(474);
 /* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(4)))
 
 /***/ }),
 
-/***/ 475:
+/***/ 474:
 /***/ (function(module, exports, __webpack_require__) {
 
 /**
@@ -222,7 +222,7 @@ module.exports = global["Scratch3VideoSensingDebug"] = __webpack_require__(475);
  */
 
 const VideoMotion = __webpack_require__(86);
-const VideoMotionView = __webpack_require__(476);
+const VideoMotionView = __webpack_require__(475);
 
 module.exports = {
     VideoMotion,
@@ -232,7 +232,7 @@ module.exports = {
 
 /***/ }),
 
-/***/ 476:
+/***/ 475:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
