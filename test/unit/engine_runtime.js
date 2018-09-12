2a13697621a3d2c27b1bd0ae2fec6fdd328bb8c9
Christopher Willis-Ford <cwillisf@media.mit.edu>
2018-09-12 17:23:01 -0400

    Add tests for `getLabelForOpcode`

--- test/unit/engine_runtime.js
+++ test/unit/engine_runtime.js
@@ -50,7 +50,7 @@ test('monitorStateDoesNotEqual', t => {
     });
     r.requestAddMonitor(prevMonitorState);
     r.requestUpdateMonitor(newMonitorDelta);
-    
+
     t.equals(false, prevMonitorState.equals(r._monitorState.get(id)));
     t.equals(String(24), r._monitorState.get(id).get('value'));
 
@@ -64,6 +64,47 @@ test('monitorStateDoesNotEqual', t => {
     t.equals(false, prevMonitorState.equals(r._monitorState.get(id)));
     t.equals(String(24), r._monitorState.get(id).value);
     t.equals(params, r._monitorState.get(id).params);
+
+    t.end();
+});
+
+test('getLabelForOpcode', t => {
+    const r = new Runtime();
+
+    const fakeExtension = {
+        id: 'fakeExtension',
+        name: 'Fake Extension',
+        blocks: [
+            {
+                info: {
+                    opcode: 'foo',
+                    json: {},
+                    text: 'Foo',
+                    xml: ''
+                }
+            },
+            {
+                info: {
+                    opcode: 'foo_2',
+                    json: {},
+                    text: 'Foo 2',
+                    xml: ''
+                }
+            }
+        ]
+    };
+
+    r._blockInfo.push(fakeExtension);
+
+    const result1 = r.getLabelForOpcode('fakeExtension_foo');
+    t.type(result1.category, 'string');
+    t.type(result1.label, 'string');
+    t.equals(result1.label, 'Fake Extension: Foo');
+
+    const result2 = r.getLabelForOpcode('fakeExtension_foo_2');
+    t.type(result2.category, 'string');
+    t.type(result2.label, 'string');
+    t.equals(result2.label, 'Fake Extension: Foo 2');
 
     t.end();
 });
