ab8d4dc5607787fce75cb4d32959758d5dbb5759
jokebookservice1 <jokebookyeye@gmail.com>
2018-09-17 22:07:26 +0100

    style(util): Address linting issues

--- src/util/cast.js
+++ src/util/cast.js
@@ -118,13 +118,12 @@ class Cast {
             // Scratch compares strings as case insensitive.
             const s1 = String(v1).toLowerCase();
             const s2 = String(v2).toLowerCase();
-            if(s1 < s2) {
-                return -1
+            if (s1 < s2) {
+                return -1;
             } else if (s1 > s2) {
-                return 1
-            } else {
-                return 0
+                return 1;
             }
+            return 0;
         }
         // Compare as numbers.
         return n1 - n2;
