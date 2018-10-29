78a0e52cc63a47f31e544d718980058ebddb801f
Katie Broida <kbroida@gmail.com>
2018-10-29 13:32:45 -0400

    Simply test and make it dynamic based on files in test directory

--- test/integration/load-extensions.js
+++ test/integration/load-extensions.js
@@ -1,50 +1,23 @@
 const path = require('path');
 const test = require('tap').test;
+const fs = require('fs');
 const readFileToBuffer = require('../fixtures/readProjectFile').readFileToBuffer;
 const VirtualMachine = require('../../src/index');
 
-const sb2Extensions = {
-    // pen: 'load-pen.sb2',
-    // music: 'load-music.sb2',
-    pen: 'dolphin-pen-test.sb2'
-    // TODO: add sample projects for video sensing, wedo 2.0, others?
-};
-
-const sb3Extensions = {
-    // pen: 'load-pen.sb3',
-    // music: 'load-music.sb3',
-    pen: 'dolphin-pen-test.sb3'
-    // TODO: add sample projects for video sensing, text-to-speech, translate, micro:bit, mindstorms
-};
-
-test('Load sb2 external extensions', async t => {
+test('Load external extensions', async t => {
     const vm = new VirtualMachine();
+    const fileList = fs.readdirSync('./test/fixtures/load-extensions/');
+    const testFiles = fileList.filter((file) => {
+        return path.extname(file) === '.sb2' || path.extname(file) === '.sb3';
+    });
 
     // Test each example extension file
-    for (const ext in sb2Extensions) {
-        const uri = path.resolve(__dirname, `../fixtures/load-extensions/${sb2Extensions[ext]}`);
+    for (const file of testFiles) {
+        const ext = file.split('-')[0];
+        const uri = path.resolve(__dirname, `../fixtures/load-extensions/${file}`);
         const project = readFileToBuffer(uri);
 
         await t.test('Confirm expected extension is installed in example sb2 projects', extTest => {
-            vm.loadProject(project)
-                .then(() => {
-                    extTest.ok(vm.extensionManager.isExtensionLoaded(ext));
-                    extTest.end();
-                });
-        });
-    }
-    t.end();
-});
-
-test('Load sb3 external extensions', async t => {
-    const vm = new VirtualMachine();
-
-    // Test each example extension file
-    for (const ext in sb3Extensions) {
-        const uri = path.resolve(__dirname, `../fixtures/load-extensions/${sb3Extensions[ext]}`);
-        const project = readFileToBuffer(uri);
-
-        await t.test('Confirm expected extension is installed in example sb3 projects', extTest => {
             vm.loadProject(project)
                 .then(() => {
                     extTest.ok(vm.extensionManager.isExtensionLoaded(ext));
