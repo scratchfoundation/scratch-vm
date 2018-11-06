329d98cabaa263c942c761da6b203fd221cac825
Eric Rosenbaum <eric.rosenbaum@gmail.com>
2018-11-06 13:04:10 -0500

    Use a gain node for volume instead of effects chain

--- src/extensions/scratch3_music/index.js
+++ src/extensions/scratch3_music/index.js
@@ -985,18 +985,18 @@ class Scratch3MusicBlocks {
             player.take();
         }
 
-        const chain = engine.createEffectChain();
-        chain.setEffectsFromTarget(util.target);
-
         // Set its pitch.
         const sampleNote = sampleArray[sampleIndex];
         const notePitchInterval = this._ratioForPitchInterval(note - sampleNote);
 
-        // Create a gain node for this note, and connect it to the sprite's
-        // simulated effectChain.
+        // Create gain nodes for this note's volume and release, and chain them
+        // to the output.
         const context = engine.audioContext;
+        const volumeGain = context.createGain();
+        volumeGain.gain.setValueAtTime(util.target.volume / 100, engine.currentTime);
         const releaseGain = context.createGain();
-        releaseGain.connect(chain.getInputNode());
+        volumeGain.connect(releaseGain);
+        releaseGain.connect(engine.getInputNode());
 
         // Schedule the release of the note, ramping its gain down to zero,
         // and then stopping the sound.
@@ -1018,7 +1018,7 @@ class Scratch3MusicBlocks {
         player.play();
         // Connect the player to the gain node.
         player.connect({getInputNode () {
-            return releaseGain;
+            return volumeGain;
         }});
         // Set playback now after play creates the outputNode.
         player.outputNode.playbackRate.value = notePitchInterval;
