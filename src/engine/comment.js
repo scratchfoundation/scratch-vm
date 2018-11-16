73e157ba7cc8edcf2938a335cdf4844e8c6a8d4c
Valerie R Young <valerie@bocoup.com>
2018-11-16 16:30:26 -0500

    Use Number instead of Cast.toNumber

--- src/engine/comment.js
+++ src/engine/comment.js
@@ -23,8 +23,8 @@ class Comment {
         this.text = text;
         this.x = x;
         this.y = y;
-        this.width = Math.max(Cast.toNumber(width), Comment.MIN_WIDTH);
-        this.height = Math.max(Cast.toNumber(height), Comment.MIN_HEIGHT);
+        this.width = Math.max(Number(width), Comment.MIN_WIDTH);
+        this.height = Math.max(Number(height), Comment.MIN_HEIGHT);
         this.minimized = minimized || false;
         this.blockId = null;
     }
