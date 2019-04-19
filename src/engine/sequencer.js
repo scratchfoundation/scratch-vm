ebdf3865d4dc17b0bb885f10fb36f517c2b9ba73
Michael "Z" Goddard <mzgoddard@gmail.com>
2019-04-19 15:50:44 -0400

    retire new threads if they have no block after the hat

--- src/engine/sequencer.js
+++ src/engine/sequencer.js
@@ -177,6 +177,12 @@ class Sequencer {
         if (!currentBlockId) {
             // A "null block" - empty branch.
             thread.popStack();
+
+            // Did the null follow a hat block?
+            if (thread.stack.length === 0) {
+                thread.status = Thread.STATUS_DONE;
+                return;
+            }
         }
         // Save the current block ID to notice if we did control flow.
         while ((currentBlockId = thread.peekStack())) {
