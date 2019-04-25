7aeff8547fedd006af971933d95e4eaa111f8264
Michael "Z" Goddard <mzgoddard@gmail.com>
2019-04-25 11:18:17 -0400

    remove Thread.isKilled, rework sequencer.stepThreads branching
    Virtual machine only blocks allow us to ensure status === DONE when a
    thread is DONE. So We do not need to test if the stack is length 0 or
    if isKilled is set. Just make sure status === DONE.

--- src/engine/runtime.js
+++ src/engine/runtime.js
@@ -1621,8 +1621,6 @@ class Runtime extends EventEmitter {
      * @param {!Thread} thread Thread object to remove from actives
      */
     _stopThread (thread) {
-        // Mark the thread for later removal
-        thread.isKilled = true;
         // Inform sequencer to stop executing that thread.
         this.sequencer.retireThread(thread);
     }
@@ -2032,9 +2030,6 @@ class Runtime extends EventEmitter {
             }
             this.profiler.start(stepProfilerId);
         }
-
-        // Clean up threads that were told to stop during or since the last step
-        this.threads = this.threads.filter(thread => !thread.isKilled);
 
         // Find all edge-activated hats, and add them to threads to be evaluated.
         for (const hatType in this._hats) {
