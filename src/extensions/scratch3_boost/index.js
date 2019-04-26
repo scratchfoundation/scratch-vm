270a445703c7c803f6314dabc89d276ccddb2aa5
Evelyn Eastmond <evhan55@gmail.com>
2019-04-26 16:35:43 -0400

    Playing with power states while we figure out the design.

--- src/extensions/scratch3_boost/index.js
+++ src/extensions/scratch3_boost/index.js
@@ -510,7 +510,9 @@ class BoostMotor {
      * @param {boolean} [resetState=true] - whether to reset the state of the motor when running this command.
      */
     turnOnFor (milliseconds, resetState = true) {
-        if (this.power === 0) return;
+        if (this.power === 0) {
+            this.turnOff(false);
+        }
 
         milliseconds = Math.max(0, milliseconds);
         if (resetState) this.status = BoostMotorState.ON_FOR_TIME;
@@ -525,10 +527,10 @@ class BoostMotor {
      * @param {boolean} [resetState=true] - whether to reset the state of the motor when running this command.
      */
     turnOnForDegrees (degrees, direction, resetState = true) {
-        if (this.power === 0) {
+        /* if (this.power === 0) {
             this._clearRotationState();
             return;
-        }
+        }*/
 
         degrees = Math.max(0, degrees);
 
