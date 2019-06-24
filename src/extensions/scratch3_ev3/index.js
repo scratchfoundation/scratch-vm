87590202cc2c8b352380003bae85d58e6e7d5cb2
Evelyn Eastmond <evhan55@gmail.com>
2019-06-23 22:42:42 -0400

    Adding 'byte to follow' comments back.

--- src/extensions/scratch3_ev3/index.js
+++ src/extensions/scratch3_ev3/index.js
@@ -41,10 +41,10 @@ const BTSendRateMax = 40;
  * @enum {number}
  */
 const Ev3Encoding = {
-    LOCAL_CONSTANT_ONE_BYTE: 0x81, // = 0b1000-001
-    LOCAL_CONSTANT_TWO_BYTES: 0x82, // = 0b1000-010
-    LOCAL_CONSTANT_FOUR_BYTES: 0x83, // = 0b1000-011
-    GLOBAL_VARIABLE_ONE_BYTE: 0xE1, // = 0b1110-001
+    LOCAL_CONSTANT_ONE_BYTE: 0x81, // = 0b1000-001, "1 byte to follow"
+    LOCAL_CONSTANT_TWO_BYTES: 0x82, // = 0b1000-010, "2 bytes to follow"
+    LOCAL_CONSTANT_FOUR_BYTES: 0x83, // = 0b1000-011, "4 bytes to follow"
+    GLOBAL_VARIABLE_ONE_BYTE: 0xE1, // = 0b1110-001, "1 byte to follow"
     GLOBAL_CONSTANT_INDEX_0: 0x20, // = 0b00100000
     GLOBAL_VARIABLE_INDEX_0: 0x60 // = 0b00110000
 };
