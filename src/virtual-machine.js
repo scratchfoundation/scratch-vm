40be416c9fa7709b43893e052832c60ff18be7eb
Chris Garrity <chrisg@media.mit.edu>
2019-08-07 13:52:30 +0100

    Add custom mime-type to exported sb3 and sprite

--- src/virtual-machine.js
+++ src/virtual-machine.js
@@ -382,6 +382,7 @@ class VirtualMachine extends EventEmitter {
 
         return zip.generateAsync({
             type: 'blob',
+            mimeType: 'application/x.scratch.sb3',
             compression: 'DEFLATE',
             compressionOptions: {
                 level: 6 // Tradeoff between best speed (1) and best compression (9)
@@ -432,6 +433,7 @@ class VirtualMachine extends EventEmitter {
 
         return zip.generateAsync({
             type: typeof optZipType === 'string' ? optZipType : 'blob',
+            mimeType: 'application/x.scratch.sprite3',
             compression: 'DEFLATE',
             compressionOptions: {
                 level: 6
