1204fc68fccb5638f79361d09042c5ec4d0061cb
DD Liu <liudi@media.mit.edu>
2019-08-22 13:10:53 -0400

    Add md5 for default asset

--- src/import/load-costume.js
+++ src/import/load-costume.js
@@ -219,6 +219,7 @@ const loadCostumeFromAsset = function (costume, runtime, optVersion) {
                 // Use default asset if original fails to load
                 costume.assetId = runtime.storage.defaultAssetId.ImageVector;
                 costume.asset = runtime.storage.get(costume.assetId);
+                costume.md5 = `${costume.assetId}.${AssetType.ImageVector.runtimeFormat}`;
                 return loadVector_(costume, runtime);
             });
     }
