fffb7b7a2a84ebe5af641533dc3a2f1091de7a43
Filip Š <filip.stamcar@hotmail.com>
2019-10-26 11:42:32 +0200

    Remove block from specmap

--- src/serialization/sb2_specmap.js
+++ src/serialization/sb2_specmap.js
@@ -1229,21 +1229,6 @@ const specMap = {
             }
         ]
     },
-    '^': {
-        opcode: 'operator_power',
-        argMap: [
-            {
-                type: 'input',
-                inputOp: 'math_number',
-                inputName: 'NUM1'
-            },
-            {
-                type: 'input',
-                inputOp: 'math_number',
-                inputName: 'NUM2'
-            }
-        ]
-    },
     'randomFrom:to:': {
         opcode: 'operator_random',
         argMap: [
