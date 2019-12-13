1dc161cea6c8201e3350ebd709c5b3a3c17f7416
nahood <nahood@ksu.edu>
2019-12-12 22:21:39 -0600

    Add row block (#11)
    * Added Add Row button. Uses visualised add row opcode.
    
    * Modified block to work with new opcode, modified addDataFileRow, getColumnAtRow, and setColumnAtRow to handle a minor error gracefully.

--- src/extensions/data_tools/index.js
+++ src/extensions/data_tools/index.js
@@ -59,6 +59,21 @@ class DataTools {
                     }
                 },
                 {
+                    opcode: 'addDataFileRow',
+                    text: formatMessage({
+                        id: 'datatools.addDataFileRow',
+                        default: 'add row to [FILENAME]',
+                        description: 'add an empty row to a file'
+                    }),
+                    blocktype: BlockType.COMMAND,
+                    arguments: {
+                        FILENAME: {
+                            type: ArgumentType.STRING,
+                            menu: 'fileMenu',
+                        }
+                    }
+                },
+                {
                     opcode: 'getColumnAtRow',
                     text: formatMessage({
                         id: 'datatools.getColumnAtRow',
@@ -206,7 +221,7 @@ class DataTools {
         let fileName = colArr[0].substring(1);
         let col = colArr.slice(1, colArr.length).join(']').substring(1);
 
-        if(!files[fileName] || ROW < 1 || ROW > files[fileName].length || !files[fileName][ROW - 1][col]) {
+        if(!files[fileName] || ROW < 1 || ROW > files[fileName].length || files[fileName][ROW - 1][col] === 'undefined') {
             return "";
         }
 
@@ -240,7 +255,7 @@ class DataTools {
         let fileName = colArr[0].substring(1);
         let col = colArr.slice(1, colArr.length).join(']').substring(1);
 
-        if(!files[fileName] || ROW < 1 || ROW > files[fileName].length || !files[fileName][ROW - 1][col]) {
+        if(!files[fileName] || ROW < 1 || ROW > files[fileName].length || files[fileName][ROW - 1][col]  === 'undefined') {
             return "";
         }
         console.log(files[fileName][ROW - 1][col]); 
@@ -257,6 +272,9 @@ class DataTools {
 
     addDataFileRow(args) {
         let { FILENAME } = args;
+        if(!FILENAME || FILENAME === ''){
+            return;
+        }
 
         let first = files[FILENAME][0];
         let newRow = {};
