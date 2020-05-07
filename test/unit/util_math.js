8700a70262afdede968eae98fe87e53f69ed911d
Joke Book <jokebookyeye@gmail.com>
2020-05-07 16:37:01 +0100

    Fix randIntWithout test case

--- test/unit/util_math.js
+++ test/unit/util_math.js
@@ -63,6 +63,6 @@ test('inclusiveRandIntWithout', t => {
     t.strictEqual(withRandomValue(1 / 6, 0, 6, 2), 1);
     t.strictEqual(withRandomValue(1.9 / 6, 0, 6, 2), 1);
 
-    t.strictEqual(withRandomValue(3 / 4, 10, 14, 10), 13);
+    t.strictEqual(withRandomValue(3 / 4, 10, 14, 10), 14);
     t.strictEqual(withRandomValue(0 / 4, 10, 14, 10), 11);
 });
