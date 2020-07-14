01e1881e74decf9b82754861380adfb50ab364ff
apple502j <33279053+apple502j@users.noreply.github.com>
2020-07-15 00:34:16 +0900

    Use missingTranslation: ignore in formatMessage

--- src/virtual-machine.js
+++ src/virtual-machine.js
@@ -1121,7 +1121,13 @@ class VirtualMachine extends EventEmitter {
      */
     setLocale (locale, messages) {
         if (locale !== formatMessage.setup().locale) {
-            formatMessage.setup({locale: locale, translations: {[locale]: messages}});
+            formatMessage.setup({
+                locale: locale,
+                translations: {
+                    [locale]: messages
+                },
+                missingTranslation: 'ignore' // Do not console.warn() missing translations (#2270)
+            });
         }
         return this.extensionManager.refreshBlocks();
     }
