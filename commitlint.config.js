582c0119887459f69937dc30bd774bdd9dec5af2
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2022-08-26 10:14:49 -0700

    build: tell commitlint to ignore release commits instead of disabling Husky during semantic-release

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional']
+    extends: ['@commitlint/config-conventional'],
+    ignores: [message => message.startsWith('chore(release):')]
 };
