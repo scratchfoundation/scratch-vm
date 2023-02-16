a24b4bd92387848b82f184c1f15ce260914bace2
adroitwhiz <adroitwhiz@protonmail.com>
2023-02-16 14:27:02 -0500

    refactor: remove stackFrame null check from isActiveThread
    If the thread's status is not STATUS_DONE, it will have a stack frame.
    Note that the inverse is not true--some threads may be STATUS_DONE but
    still have a stack frame. We thus choose to keep only the STATUS_DONE
    check.

--- src/engine/runtime.js
+++ src/engine/runtime.js
@@ -1689,9 +1689,7 @@ class Runtime extends EventEmitter {
      */
     isActiveThread (thread) {
         return (
-            (
-                thread.stackFrame !== null &&
-                thread.status !== Thread.STATUS_DONE) &&
+            thread.status !== Thread.STATUS_DONE &&
             this.threads.indexOf(thread) > -1);
     }
 
