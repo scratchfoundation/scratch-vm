73a306b661e48f99ea50b107f9230663e651f090
adroitwhiz <adroitwhiz@protonmail.com>
2023-03-04 22:42:52 -0500

    refactor: initialize thread stack to first block
    We were manually setting the first block everywhere we constructed a new
    Thread. Instead, just pass it in.

--- src/engine/runtime.js
+++ src/engine/runtime.js
@@ -1654,7 +1654,6 @@ class Runtime extends EventEmitter {
             this.monitorBlocks :
             target.blocks;
 
-        thread.pushStack(id);
         this.threads.push(thread);
         return thread;
     }
@@ -1672,7 +1671,6 @@ class Runtime extends EventEmitter {
         newThread.stackClick = thread.stackClick;
         newThread.updateMonitor = thread.updateMonitor;
         newThread.blockContainer = thread.blockContainer;
-        newThread.pushStack(thread.topBlock);
         const i = this.threads.indexOf(thread);
         if (i > -1) {
             this.threads[i] = newThread;
