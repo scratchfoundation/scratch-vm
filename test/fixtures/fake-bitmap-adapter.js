b49cfa4c6bdddf4b258dd6123860cac7b240219a
Elliot Roe <Elliot.Roe@Honeywell.com>
2023-03-08 16:03:41 -0500

    Deleted uneeded test files

--- test/fixtures/fake-bitmap-adapter.js
+++
@@ -1,7 +0,0 @@
-const FakeBitmapAdapter = require('scratch-svg-renderer').BitmapAdapter;
-
-FakeBitmapAdapter.prototype.resize = function (canvas) {
-    return canvas;
-};
-
-module.exports = FakeBitmapAdapter;
