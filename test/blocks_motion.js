76ea04b02d3da6257683ee0f1451977285a66e3a
Elliot Roe <Elliot.Roe@Honeywell.com>
2023-03-09 13:55:37 -0500

    All motion blocks supported with major exceptions

--- test/blocks_motion.js
+++ test/blocks_motion.js
@@ -42,13 +42,14 @@ describe('Runtime Exec Primitives', () => {
 
         it('Go To', async () => {
             const rt = new Runtime();
-            const sprite1 = new Sprite(null, rt);
+            const sprite1 = new Sprite(rt);
             const target1 = new RenderedTarget(sprite1, rt);
             rt.addTarget(target1);
             
-            const sprite2 = new Sprite(null, rt);
+            const sprite2 = new Sprite(rt);
             const target2 = new RenderedTarget(sprite2, rt);
             target2.name = 'destination_target';
+            sprite2.name = 'destination_target';
             target2.setXY(5, 2);
             rt.addTarget(target2);
 
@@ -74,162 +75,208 @@ describe('Runtime Exec Primitives', () => {
 
             const direction = 90;
 
-            const retVal = await rt.execBlockPrimitive(target.id, 'motion_turnright', {DEGREES: direction}, 'test_token');
+            const retVal = await rt.execBlockPrimitive(
+                target.id, 'motion_turnright', {DEGREES: direction}, 'test_token');
             expect(spy).to.have.been.calledWithExactly(target.drawableID, 180, [100, 100]);
             expect(retVal).to.equal(undefined);
         });
 
-        it('Go To XY', async () => {
+        it('Turn Left', async () => {
             const rt = new Runtime();
             const sprite = new Sprite(null, rt);
             const target = new RenderedTarget(sprite, rt);
-            const targetId = target.id;
             rt.addTarget(target);
 
-            const retVal = await rt.execBlockPrimitive(targetId, 'motion_movesteps', {STEPS: 10}, 'test_token');
+            const spy = sinon.spy();
+            const mockRenderer = {
+                updateDrawableDirectionScale: spy
+            };
 
-            expect(target.x).to.equal(10);
-            expect(target.y).to.equal(0);
+            target.renderer = mockRenderer;
+
+            const direction = 90;
+
+            const retVal = await rt.execBlockPrimitive(
+                target.id, 'motion_turnleft', {DEGREES: direction}, 'test_token');
+            expect(spy).to.have.been.calledWithExactly(target.drawableID, 0, [100, 100]);
             expect(retVal).to.equal(undefined);
         });
 
-        it('Go To XY', async () => {
+        it('Point In Direction', async () => {
             const rt = new Runtime();
             const sprite = new Sprite(null, rt);
             const target = new RenderedTarget(sprite, rt);
-            const targetId = target.id;
             rt.addTarget(target);
 
-            const retVal = await rt.execBlockPrimitive(targetId, 'motion_movesteps', {STEPS: 10}, 'test_token');
+            const spy = sinon.spy();
+            const mockRenderer = {
+                updateDrawableDirectionScale: spy
+            };
 
-            expect(target.x).to.equal(10);
-            expect(target.y).to.equal(0);
+            target.renderer = mockRenderer;
+
+            const direction = 180;
+
+            const retVal = await rt.execBlockPrimitive(
+                target.id, 'motion_pointindirection', {DIRECTION: direction}, 'test_token');
+            expect(spy).to.have.been.calledWithExactly(target.drawableID, 180, [100, 100]);
             expect(retVal).to.equal(undefined);
         });
 
-        it('Go To XY', async () => {
+
+        it('pointTowards', async () => {
             const rt = new Runtime();
-            const sprite = new Sprite(null, rt);
-            const target = new RenderedTarget(sprite, rt);
-            const targetId = target.id;
-            rt.addTarget(target);
+            const sprite1 = new Sprite(rt);
+            const target1 = new RenderedTarget(sprite1, rt);
+            rt.addTarget(target1);
+            
+            const sprite2 = new Sprite(rt);
+            const target2 = new RenderedTarget(sprite2, rt);
+            target2.name = 'destination_target';
+            sprite2.name = 'destination_target';
+            target2.setXY(5, 5);
+            rt.addTarget(target2);
 
-            const retVal = await rt.execBlockPrimitive(targetId, 'motion_movesteps', {STEPS: 10}, 'test_token');
+            const spy = sinon.spy();
+            const mockRenderer = {
+                updateDrawableDirectionScale: spy
+            };
 
-            expect(target.x).to.equal(10);
-            expect(target.y).to.equal(0);
+            target1.renderer = mockRenderer;
+
+            const retVal = await rt.execBlockPrimitive(
+                target1.id, 'motion_pointtowards', {TO: target2.name}, 'test_token');
+
+            expect(spy).to.have.been.calledWithExactly(target1.drawableID, 135, [100, 100]);
             expect(retVal).to.equal(undefined);
         });
 
-        it('Go To XY', async () => {
+        it('Glide', async () => {
             const rt = new Runtime();
             const sprite = new Sprite(null, rt);
             const target = new RenderedTarget(sprite, rt);
             const targetId = target.id;
             rt.addTarget(target);
 
-            const retVal = await rt.execBlockPrimitive(targetId, 'motion_movesteps', {STEPS: 10}, 'test_token');
+            // eslint-disable-next-line no-unused-vars
+            const retVal = await rt.execBlockPrimitive(targetId, 'motion_glidesecstoxy', {X: 10, Y: 10, SECS: 1}, 'test_token');
 
-            expect(target.x).to.equal(10);
-            expect(target.y).to.equal(0);
+            // NOTE: Not implemented yet
+            expect(true).to.equal(false);
+        });
+
+        it('Glide To', async () => {
+            const rt = new Runtime();
+            const sprite1 = new Sprite(rt);
+            const target1 = new RenderedTarget(sprite1, rt);
+            rt.addTarget(target1);
+            
+            const sprite2 = new Sprite(rt);
+            const target2 = new RenderedTarget(sprite2, rt);
+            target2.name = 'destination_target';
+            sprite2.name = 'destination_target';
+            target2.setXY(5, 5);
+            rt.addTarget(target2);
+
+            // eslint-disable-next-line no-unused-vars
+            const retVal = await rt.execBlockPrimitive(target1.id, 'motion_glideto', {TO: target2.name}, 'test_token');
+
+            // NOTE: Not implemented yet
+            expect(true).to.equal(false);
+        });
+
+        it('If On Edge Bounce', async () => {
+            const rt = new Runtime();
+            const sprite = new Sprite(null, rt);
+            const target = new RenderedTarget(sprite, rt);
+            const targetId = target.id;
+            rt.addTarget(target);
+
+            // eslint-disable-next-line no-unused-vars
+            const retVal = await rt.execBlockPrimitive(targetId, 'motion_ifonedgebounce', {}, 'test_token');
+
+            // NOTE: Not implemented yet
+            expect(true).to.equal(false);
+        });
+
+        it('Set Rotation Style', async () => {
+            const rt = new Runtime();
+            const sprite = new Sprite(null, rt);
+            const target = new RenderedTarget(sprite, rt);
+            const targetId = target.id;
+            rt.addTarget(target);
+
+            // eslint-disable-next-line no-unused-vars
+            const retVal = await rt.execBlockPrimitive(
+                targetId, 'motion_setrotationstyle', {STYLE: RenderedTarget.ROTATION_STYLE_NONE}, 'test_token');
+
+            // NOTE: Not implemented yet
+            expect(true).to.equal(false);
+        });
+
+        it('Change X', async () => {
+            const rt = new Runtime();
+            const sprite = new Sprite(null, rt);
+            const target = new RenderedTarget(sprite, rt);
+            rt.addTarget(target);
+
+            oldX = target.x;
+            dx = 10;
+            oldY = target.y;
+
+            const retVal = await rt.execBlockPrimitive(target.id, 'motion_changexby', {DX: dx}, 'test_token');
+
+            expect(target.x).to.equal(oldX + dx);
+            expect(target.y).to.equal(oldY);
             expect(retVal).to.equal(undefined);
         });
 
-        it('Go To XY', async () => {
+        it('Change Y', async () => {
             const rt = new Runtime();
             const sprite = new Sprite(null, rt);
             const target = new RenderedTarget(sprite, rt);
-            const targetId = target.id;
             rt.addTarget(target);
 
-            const retVal = await rt.execBlockPrimitive(targetId, 'motion_movesteps', {STEPS: 10}, 'test_token');
+            oldX = target.x;
+            oldY = target.y;
+            dy = 10;
 
-            expect(target.x).to.equal(10);
-            expect(target.y).to.equal(0);
+            const retVal = await rt.execBlockPrimitive(target.id, 'motion_changeyby', {DY: dy}, 'test_token');
+
+            expect(target.x).to.equal(oldX);
+            expect(target.y).to.equal(oldY + dy);
             expect(retVal).to.equal(undefined);
         });
 
-        it('Go To XY', async () => {
+        it('Set X', async () => {
             const rt = new Runtime();
             const sprite = new Sprite(null, rt);
             const target = new RenderedTarget(sprite, rt);
-            const targetId = target.id;
             rt.addTarget(target);
 
-            const retVal = await rt.execBlockPrimitive(targetId, 'motion_movesteps', {STEPS: 10}, 'test_token');
+            const eX = 10;
+            const oldY = target.y;
 
-            expect(target.x).to.equal(10);
-            expect(target.y).to.equal(0);
+            const retVal = await rt.execBlockPrimitive(target.id, 'motion_setx', {X: eX}, 'test_token');
+
+            expect(target.x).to.equal(eX);
+            expect(target.y).to.equal(oldY);
             expect(retVal).to.equal(undefined);
         });
 
-        it('Go To XY', async () => {
+        it('Set Y', async () => {
             const rt = new Runtime();
             const sprite = new Sprite(null, rt);
             const target = new RenderedTarget(sprite, rt);
-            const targetId = target.id;
             rt.addTarget(target);
 
-            const retVal = await rt.execBlockPrimitive(targetId, 'motion_movesteps', {STEPS: 10}, 'test_token');
+            const eY = 10;
+            const oldX = target.x;
 
-            expect(target.x).to.equal(10);
-            expect(target.y).to.equal(0);
-            expect(retVal).to.equal(undefined);
-        });
+            const retVal = await rt.execBlockPrimitive(target.id, 'motion_sety', {Y: eY}, 'test_token');
 
-        it('Go To XY', async () => {
-            const rt = new Runtime();
-            const sprite = new Sprite(null, rt);
-            const target = new RenderedTarget(sprite, rt);
-            const targetId = target.id;
-            rt.addTarget(target);
-
-            const retVal = await rt.execBlockPrimitive(targetId, 'motion_movesteps', {STEPS: 10}, 'test_token');
-
-            expect(target.x).to.equal(10);
-            expect(target.y).to.equal(0);
-            expect(retVal).to.equal(undefined);
-        });
-
-        it('Go To XY', async () => {
-            const rt = new Runtime();
-            const sprite = new Sprite(null, rt);
-            const target = new RenderedTarget(sprite, rt);
-            const targetId = target.id;
-            rt.addTarget(target);
-
-            const retVal = await rt.execBlockPrimitive(targetId, 'motion_movesteps', {STEPS: 10}, 'test_token');
-
-            expect(target.x).to.equal(10);
-            expect(target.y).to.equal(0);
-            expect(retVal).to.equal(undefined);
-        });
-
-        it('Go To XY', async () => {
-            const rt = new Runtime();
-            const sprite = new Sprite(null, rt);
-            const target = new RenderedTarget(sprite, rt);
-            const targetId = target.id;
-            rt.addTarget(target);
-
-            const retVal = await rt.execBlockPrimitive(targetId, 'motion_movesteps', {STEPS: 10}, 'test_token');
-
-            expect(target.x).to.equal(10);
-            expect(target.y).to.equal(0);
-            expect(retVal).to.equal(undefined);
-        });
-
-        it('Go To XY', async () => {
-            const rt = new Runtime();
-            const sprite = new Sprite(null, rt);
-            const target = new RenderedTarget(sprite, rt);
-            const targetId = target.id;
-            rt.addTarget(target);
-
-            const retVal = await rt.execBlockPrimitive(targetId, 'motion_movesteps', {STEPS: 10}, 'test_token');
-
-            expect(target.x).to.equal(10);
-            expect(target.y).to.equal(0);
+            expect(target.x).to.equal(oldX);
+            expect(target.y).to.equal(eY);
             expect(retVal).to.equal(undefined);
         });
     });
