ecc25d84b68aee29c5a0b3a12af35ceeadf54dae
Roe, Elliot Benjamin <eroe7@gatech.edu>
2023-04-09 17:05:02 -0400

    Rullup Conifg Modified for successful bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,43 @@
 // rollup.config.js
 import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json'
+import alias from '@rollup/plugin-alias';
+import replace from '@rollup/plugin-replace';
+import { base64 } from "rollup-plugin-base64";
 
 export default {
 	input: 'src/virtual-machine.mjs',
 	output: {
 		file: 'dist/bundle.js',
-		format: 'esm'
+		strict: false,
+		format: 'esm',
+		inlineDynamicImports: true,
 	},
 	plugins: [
 		resolve({
 			moduleDirectories: ['node_modules']
 		}),
-		babel({ babelHelpers: 'bundled' })
+		replace({
+			'base64-loader!': '',
+			delimiters: ['', ''],
+			preventAssignment: true
+		}),
+		babel({ babelHelpers: 'bundled' }),
+		json(),
+		alias({
+			entries: [
+				// Mini log is no longer maintained
+			  { find: 'minilog', replacement: 'pino-minilog' },
+			]
+		}),
+		base64({
+			include: ['**/*.otf','**/*.ttf'],
+		}),
+		commonjs({
+			include: /node_modules/,
+			requireReturnsDefault: 'auto', 
+		}),
 	],
 };
