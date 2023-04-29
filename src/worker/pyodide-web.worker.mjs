2d57e4320cf3ad05644c7d53b516e845beeb72bd
Roe, Elliot Benjamin <eroe7@gatech.edu>
2023-04-29 19:30:14 -0400

    More verbose output for debugging

--- src/worker/pyodide-web.worker.mjs
+++ src/worker/pyodide-web.worker.mjs
@@ -91,6 +91,8 @@ function _run(pythonScript,  targets) {
   //await self.pyodide.loadPackagesFromImports(python);
   _postStatusMessage(WorkerMessages.ToVM.PythonLoading)
 
+  console.log(self.pyodide);
+
   // This is load each async function into the global scope of the pyodide instance
   self.pyodide.runPython(pythonScript);
   _postStatusMessage(WorkerMessages.ToVM.PythonRunning);
