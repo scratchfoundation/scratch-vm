15bad61f874b2c485a605ac7c9b39bee2c00c530
wendyqueen <wanqiuzhengcs@gmail.com>
2025-06-16 21:07:46 +0800

    opt: support config TTS and translate service endpoint via env

--- src/extensions/scratch3_text2speech/index.js
+++ src/extensions/scratch3_text2speech/index.js
@@ -27,7 +27,7 @@ const blockIconURI = 'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNv
  * The url of the synthesis server.
  * @type {string}
  */
-const SERVER_HOST = 'https://synthesis-service.scratch.mit.edu';
+const SERVER_HOST = process.env.SCRATCH_TTS_ENDPOINT || 'https://synthesis-service.scratch.mit.edu';
 
 /**
  * How long to wait in ms before timing out requests to synthesis server.
--- src/extensions/scratch3_translate/index.js
+++ src/extensions/scratch3_translate/index.js
@@ -24,7 +24,7 @@ const blockIconURI = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAFAAAABQCAYA
  * The url of the translate server.
  * @type {string}
  */
-const serverURL = 'https://translate-service.scratch.mit.edu/';
+const serverURL = process.env.SCRATCH_TRANSLATE_ENDPOINT || 'https://translate-service.scratch.mit.edu/';
 
 /**
  * How long to wait in ms before timing out requests to translate server.
